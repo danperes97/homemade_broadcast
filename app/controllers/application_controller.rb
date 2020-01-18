c9fd8d2156270b2047d38fdbde0e69105d4b7800
DanPeres <danilo.peres@gympass.com>
2020-01-18 16:05:56 -0300

    Fix carrierwave

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :set_viewer
-  protect_from_forgery with: :exception, prepend: true
-
 
   def set_viewer
     @viewer = Viewer.find_or_create_by!(ip: request.remote_ip)
