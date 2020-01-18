da6afcf1d0bb263f5acf5b0afc59aad5decfbfc5
DanPeres <danilo.peres@gympass.com>
2020-01-18 16:20:37 -0300

    Remove csrf on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_viewer
+  protect_from_forgery except: [:sign_in, :sign_up]
 
   def set_viewer
     @viewer = Viewer.find_or_create_by!(ip: request.remote_ip)
