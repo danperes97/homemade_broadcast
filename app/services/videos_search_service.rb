fa8ae73e2528064e01ae3da443e25bf6e21c5b12
DanPeres <danilo.peres@gympass.com>
2020-02-01 15:17:26 -0300

    Refactor search service

--- app/services/videos_search_service.rb
+++ app/services/videos_search_service.rb
@@ -1,9 +1,19 @@
 class VideosSearchService
   def self.search(options = {})
     if options[:query].present?
-      Video.where("lower(title) LIKE ?", "%#{options[:query].downcase}%")
+      search_by_query(options[:query])
     elsif options[:category].present?
-      Video.joins(:categories).where(categories: { tag_name: options[:category] })
+      search_by_category(options[:category])
     end
+  end
+
+  private
+
+  def self.search_by_query(query)
+    Video.where("lower(title) LIKE ?", "%#{query.downcase}%")
+  end
+
+  def self.search_by_category(category)
+    Video.joins(:categories).where(categories: { tag_name: category })
   end
 end
